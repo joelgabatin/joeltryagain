de9e27e25207784c1221a62859a05966b1c3193c
joelgabatin <joelgabatin47@gmail.com>
2023-11-17 01:50:24 +0800

    database
    database connection

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
 from flask import Flask, render_template, request, flash
+import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
+# MySQL Configuration
+db_config = {
+    'host': 'x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'user': 'of0zxxndddv53clf',
+    'password': 'rhyfmihnp62cmw1x',
+    'database': 'e5wmoflyt8aawue7'
+}
+
+# Function to Connect to MySQL
+def connect_to_mysql():
+    conn = mysql.connector.connect(**db_config)
+    return conn
+
 @app.route("/hello")
 @app.route("/")
 def index():
-	flash("what's your name?")
-	return render_template("index.html")
+    flash("what's your name?")
+    return render_template("index.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+    conn = connect_to_mysql()
+    cursor = conn.cursor()
+
+    # Fetching name from tbl_users
+    select_query = "SELECT name FROM tbl_users"
+    cursor.execute(select_query)
+    result = cursor.fetchone()  # Fetches the first row
+
+    if result:
+        name = result[0]  # Extracting the name from the fetched row
+        flash(f"Hi {name}, great to see you!")
+    else:
+        flash("No name found in the database.")
+
+    conn.close()
+    return render_template("index.html")
